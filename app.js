9cd9fb04143ebfe2b9655bbefb5a2e146b586b1d
erembedded <addaembedded@gmail.com>
2025-05-14 20:38:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,25 @@ function login() {
   const email = document.getElementById("email").value;
   const pass = document.getElementById("password").value;
 
+  auth.createUserWithEmailAndPassword(email, pass)
+    .then(() => {
+      document.getElementById("message").textContent = "Account created. Redirecting...";
+      // Wait for auth state change
+      auth.onAuthStateChanged(user => {
+        if (user) {
+          window.location.href = "dashboard.html";
+        }
+      });
+    })
+    .catch(err => {
+      document.getElementById("message").textContent = err.message;
+    });
+}
+
+function login1() {
+  const email = document.getElementById("email").value;
+  const pass = document.getElementById("password").value;
+
   auth.signInWithEmailAndPassword(email, pass)
     .then(() => {
       window.location.href = "dashboard.html"; // redirect after login
